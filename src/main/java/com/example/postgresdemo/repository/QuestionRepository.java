585cab5c4140ffd588c1652f3d66f0677d49ebcf
WojciechRekosiewicz <wojtek24a@gmail.com>
2019-02-27 18:46:58 +0100

    questionRepository done

--- src/main/java/com/example/postgresdemo/repository/QuestionRepository.java
+++ src/main/java/com/example/postgresdemo/repository/QuestionRepository.java
@@ -1,4 +1,9 @@
 package com.example.postgresdemo.repository;
 
-public interface QuestionRepository {
+import com.example.postgresdemo.model.Question;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface QuestionRepository extends JpaRepository<Question, Long> {
 }
